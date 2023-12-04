d215c933faf8511a8edafd65cd0769f36700da55
Youss0203 <youssefhalibi003@gmail.com>
2023-12-04 18:34:08 +0100

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const {createApp}=Vue
+
+createApp({
+    data(){
+       return{
+        toDoList:[
+            {
+                text: "comprare le scarpe da calcio nuove",
+                done: true
+            },
+            {
+                text: "andare a correre",
+                done: false
+            },
+            {
+                text: "imparare a sviluppare con js",
+                done: true
+            },
+            {
+                text: "giocare alla play con gli amici",
+                done: false
+            },
+            {
+                text: "visitare un città europea",
+                done: false
+            },
+        ]
+
+       } 
+    },
+    methods:{
+        
+    }
+}).mount('#app')
